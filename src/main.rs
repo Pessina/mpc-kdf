b112397360b14d3835d18260799f5837c00c26d3
Felipe Pessina <fs.pessina@gmail.com>
2024-02-25 15:59:39 +0400

    Fix public key type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use near_sdk::PublicKey;
+
 mod kdf;
 mod types;
 mod util;
@@ -7,18 +9,13 @@ fn main() {
     use k256::{AffinePoint, EncodedPoint};
     use k256::elliptic_curve::sec1::FromEncodedPoint;
     
-    let public_key_str = "secp256k1:37aFybhUHCxRdDkuCcB3yHzxqK7N8EQ745MujyAQohXSsYymVeHzhLxKvZ2qYeRHf3pGFiAsxqFJZjpF9gP2JV5u";
+    let public_key: PublicKey = "secp256k1:37aFybhUHCxRdDkuCcB3yHzxqK7N8EQ745MujyAQohXSsYymVeHzhLxKvZ2qYeRHf3pGFiAsxqFJZjpF9gP2JV5u".parse().unwrap();
     let account_id: near_primitives::types::AccountId = "felipe-sandbox.testnet".parse().unwrap();
     let path = ",ethereum,near.org";
 
-    let mut pk_bytes = vec![0x04];
-    let hex_str = public_key_str.split(':').nth(1).expect("Invalid format for public_key_str");
-
-    // Decode the base58 string
-    let decoded_bytes = bs58::decode(hex_str).into_vec().expect("Failed to decode base58");
-
+    let mut pk_bytes= vec![0x04];
     // Extend `pk_bytes` with the decoded bytes
-    pk_bytes.extend_from_slice(&decoded_bytes);
+    pk_bytes.extend_from_slice(&public_key.as_bytes()[1..]);
 
     let point = EncodedPoint::from_bytes(pk_bytes).unwrap();
     let public_key = AffinePoint::from_encoded_point(&point).unwrap();
